d6b6d282d0a8e76e445249cb539e762fc8e16776
Yves Orton <demerphq@gmail.com>
2016-08-25 16:33:29 +0200

    v0.16 fix scaling issue, and minor defaulting issue

--- lib/IPC/ConcurrencyLimit/Lock.pm
+++ lib/IPC/ConcurrencyLimit/Lock.pm
@@ -3,7 +3,7 @@ use 5.008001;
 use strict;
 use warnings;
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 sub id { $_[0]->{id} }
 
 sub heartbeat { 1 }
--- lib/IPC/ConcurrencyLimit/Lock/Flock.pm
+++ lib/IPC/ConcurrencyLimit/Lock/Flock.pm
@@ -8,7 +8,7 @@ use File::Spec;
 use Fcntl qw(:DEFAULT :flock);
 use IO::File ();
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 use IPC::ConcurrencyLimit::Lock;
 our @ISA = qw(IPC::ConcurrencyLimit::Lock);
 
